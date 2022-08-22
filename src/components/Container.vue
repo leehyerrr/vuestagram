87ef89ecda180fc6d306ad4eeb5910eb12dbcda4
leehyerrr <leehyerrr@gmail.com>
2022-08-22 17:53:47 +0900

    220822 첫번째

---
+++ src/components/Container.vue
@@ -0,0 +1,18 @@
+<template>
+  <Post/>
+  <Post/>
+  <Post/>
+</template>
+
+<script>
+import Post from "./Post"
+export default {
+  components:{
+    Post
+  }
+}
+</script>
+
+<style>
+
+</style>
